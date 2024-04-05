7d0d61c33164eb2bafc73d7775e469ceebcbc3f0
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-04-05 15:01:27 -0300

    função createAccount responsável pela criação da conta

--- index.js
+++ index.js
@@ -18,7 +18,17 @@ function operation() {
 ])
 .then((answers) => {
     const action = answers['action']
-    console.log(action)
+    
+    if(action === 'Criar Conta') {
+        createAccount()
+    }
 })
 .catch((err) => {console.log(err)})
+}
+
+// create an account 
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
 }
