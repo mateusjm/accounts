65067d0b518136629208fc73aa56403092673a41
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-04-05 19:29:25 -0300

    função buildCreate / criação de diretório accounts e arquivo accountName

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ operation()
 
 function operation() {
     inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'action',
-            message: 'O que deseja fazer?',
-            choices: ['Criar Conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
-        }
-])
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'O que deseja fazer?',
+                choices: ['Criar Conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
+            }
+    ])
 .then((answers) => {
     const action = answers['action']
-    
+
     if(action === 'Criar Conta') {
         createAccount()
     }
@@ -30,5 +30,48 @@ function operation() {
 
 function createAccount() {
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
-    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir:'))
+
+    buildAccount()
+}
+
+function buildAccount() {
+
+    inquirer
+        .prompt([
+            {
+                name: 'accountName',
+                message: 'Digite um nome para a sua conta:'
+            }
+]).then((answer) =>{
+    const accountName = answer['accountName']
+
+    // create diretory 'accounts'
+    if(!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts')
+    }
+
+    if(fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+            chalk.bgRed.black('Esta conta já existe, escolha outro nome!')
+        )
+        buildAccount()
+        return
+    }
+
+    fs.writeFileSync(
+        `accounts/${accountName}.json`, 
+        '{"balance": 0}',
+        function (err) {
+            console.log(err)
+        }
+    )
+
+    console.log(chalk.green('Parabéns, a sua conta foi criada!'))
+    operation()
+
+}).catch((err)=> {
+    console.log(err)
+})
+
 }
