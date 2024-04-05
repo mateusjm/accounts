1d1da6b9f0e27604e7988339ba64ed8514c40456
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-04-05 14:53:28 -0300

    criando a promisse operation responsável pela inicialização do sistema e ações

--- index.js
+++ index.js
@@ -4,5 +4,21 @@ const chalk = require('chalk')
 
 // modulos internos
 const fs = require('fs')
+operation()
 
-console.log('Iniciamos o Accounts!')
+function operation() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'O que deseja fazer?',
+            choices: ['Criar Conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
+        }
+])
+.then((answers) => {
+    const action = answers['action']
+    console.log(action)
+})
+.catch((err) => {console.log(err)})
+}
